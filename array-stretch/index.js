45b93e42b77bab2d7d1315e4aedb0272929fb703
Louie Camacho <louiecamacho@gmail.com>
2026-01-29 09:34:10 -0600

    add array stretch

--- array-stretch/index.js
+++ array-stretch/index.js
@@ -1,11 +1,19 @@
 const bunnyNames = [`Sunny`, `Toki`, `Gigi`, `Frodo`, `Samwise`, `Merry`]
 
 
-const addIsTheBest = (array) => {
+const addIsTheBest = (names) => {
   
-  for(let i=0; i<array.length; i++) {
-    console.log(array[i]+` is the best!`);
-  }
+  // for(let i=0; i<array.length; i++) {
+  //   console.log(array[i]+` is the best!`);
+  // }
+  
+  const statement = [];
+  for (let name of names) { 
+    console.log(`${name} is the best`);
+    statement.push(`${name} is the best`);
+    console.log(statement)
+  } 
+  return statement
 }
 
 addIsTheBest(bunnyNames);
